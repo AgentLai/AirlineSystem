870236e7fca4f955b1bc477f4551834ddc2d84ce
AgentLai <124452442+AgentLai@users.noreply.github.com>
2024-07-23 10:58:31 +0800

    Add files via upload

---
+++ Airline.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Airline{
+	public static void main(String[] args){
+		
+	}
+	
+	public static void addEdge(){
+		
+	}
+	
+	public static void addVertex(){
+		
+	}
+	
+	public static void removeEdge(){
+		
+	}
+	
+	public static void removeVertex(){
+		
+	}
+	
+	public static void createGraph(){
+		
+	}
+	
+	public static void viewFlightNetwork(){
+		
+	}
+	
+	public static void searchAirport(){
+		
+	}
