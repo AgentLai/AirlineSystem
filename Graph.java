0c1127261bd9d26e5b0cfbdf1d7997e8f673887a
AgentLai <124452442+AgentLai@users.noreply.github.com>
2024-07-23 13:14:52 +0800

    Added Graph class and updated Airline class

---
+++ Graph.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Graph {
+    private Map<String, List<String>> adjacencyList;
+
+    public Graph() {
+        adjacencyList = new HashMap<>();
+    }
+
+    public void addVertex(String vertex) {
+        adjacencyList.putIfAbsent(vertex, new ArrayList<>());
+    }
+
+    public void removeVertex(String vertex) {
+        adjacencyList.values().forEach(e -> e.remove(vertex));
+        adjacencyList.remove(vertex);
+    }
+
+    public void addEdge(String vertex1, String vertex2) {
+        adjacencyList.get(vertex1).add(vertex2);
+        adjacencyList.get(vertex2).add(vertex1);
+    }
+
+    public void removeEdge(String vertex1, String vertex2) {
+        List<String> edgesVertex1 = adjacencyList.get(vertex1);
+        List<String> edgesVertex2 = adjacencyList.get(vertex2);
+        
+	        if (edgesVertex1 != null) {
+	            edgesVertex1.remove(vertex2);
+	        }
+	        if (edgesVertex2 != null) {
+	            edgesVertex2.remove(vertex1);
+	        }
+    }
+
+    public Set<String> getVertices() {
+        return adjacencyList.keySet();
+    }
+
+    public List<String> getEdges(String vertex) {
+        return adjacencyList.get(vertex);
+    }
+}
