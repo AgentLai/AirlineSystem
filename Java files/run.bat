d028ffef1ab755f8b51f7d5c49dea0f3d4b5cbb5
AgentLai <Agentlaiyishen@gmail.com>
2024-08-24 18:55:16 +0800

    Update

--- Java files/run.bat
+++ Java files/run.bat
@@ -1,5 +1,6 @@
 @echo off
-set PATH_TO_FX="C:\Users\user\javafx-sdk-22.0.2\lib"
-javac --module-path %PATH_TO_FX% --add-modules javafx.controls,javafx.fxml *.java
-java --module-path %PATH_TO_FX% --add-modules javafx.controls,javafx.fxml AirlineSystem
-pause
+REM Compile the Java files
+javac --module-path "D:\Programming\Tools\Random Programming Junk\openjfx-22.0.2_windows-x64_bin-sdk\javafx-sdk-22.0.2\lib" --add-modules javafx.controls,javafx.fxml *.java
+
+REM Run the compiled Java program
+java --module-path "D:\Programming\Tools\Random Programming Junk\openjfx-22.0.2_windows-x64_bin-sdk\javafx-sdk-22.0.2\lib" --add-modules javafx.controls,javafx.fxml AirlineSystem
